7e608a949173da4aa093dc64bc982dc86dd8b66d
denbenki <85946666+denbenki@users.noreply.github.com>
2021-07-21 17:46:52 +0200

    Add files via upload

--- Main_Pack/gradient_ascendor.py
+++ Main_Pack/gradient_ascendor.py
@@ -20,18 +20,6 @@ def graph_position_ascendor(graph, learning_rate = 0.01):
 
     # This is going to be a little ugly, but I'll try to improve the syntax later
     positions_var_list = optimize_positions(graph, learning_rate)
-    positions_list = []
 
-    i = 0
-    while i < len(positions_var_list):
-        # Check the optimized positions and add them to a list of lists: sub-list  = coordinates of nodes
-
-        a = positions_var_list[i].numpy()
-        b = positions_var_list[i+1].numpy()
-        c = positions_var_list[i+2].numpy()
-        positions_list.append([a, b, c])
-
-        i += 3
-
-    positions_dict = {j: positions_list[j] for j in range(0, len(positions_list))}  # Turn it in to a dict
-    plot_threed_graph(positions_dict, graph)
+    positions_dict = {j: positions_var_list[j] for j in range(0, len(positions_var_list))}  # Turn it in to a dict
+    #plot_threed_graph(positions_dict, graph)
--- testing.py
+++ testing.py
@@ -1,10 +1,31 @@
+import time
+
 import Main_Pack
 import random
+import networkx as nx
+import tensorflow as tf
+import numpy
+import matplotlib.pyplot as plt
 
 from Main_Pack import ascend_gradient
 from Main_Pack import connected_graph_generator
 from Main_Pack import gradient_ascendor
 from Main_Pack import plot_3D_graph
 
-graph = connected_graph_generator.gnp_random_connected_graph(3, 0.33, 10)
+# node_time_list = []
+# node_amount_list = []
+# max_amount_of_nodes = 50
+# for i in range(1, max_amount_of_nodes + 1):
+
+i = 5
+a0 = time.time()
+graph = connected_graph_generator.gnp_random_connected_graph(i, 1/i, 10)
 gradient_ascendor.graph_position_ascendor(graph)
+b0 = time.time()
+print(b0-a0)
+
+# node_time_list.append(b0-a0)
+# node_amount_list.append(i)
+
+# plt.plot(node_amount_list, node_time_list)
+# plt.show()
